5b6ed1932441a213e976aa0bbc30ee6e9af34bf5
BlackHebrew <36684189+Hsppie@users.noreply.github.com>
2022-11-30 05:27:27 +0300

    fixed

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for number in range(100):
     if number == 99:
-        print("{}".format(number), end="")
+        print("{}".format(number))
     else:
         print("{number:02},".format(number=number), end=" ")
 
