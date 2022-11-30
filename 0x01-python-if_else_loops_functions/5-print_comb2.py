f3473bd04ee147ca8bc8deab1bd602b950bc4e98
Hsppie <wambimicheal@gmail.com>
2022-11-29 18:14:14 -0800

    fixed error

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for number in range(99):
-    print("{number:02d},".format(number=number), end=" ")
-print(99)
+for number in range(100):
+    if number == 99:
+        print("{}".format(number), end="")
+    else:
+        print("{number:02d},".format(number=number), end=" ")
+
